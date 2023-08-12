f4eda392c0265a67e220d6f0fbb8fcfcae05d7de
alvintfl <alvintanfulong@gmail.com>
2023-08-13 00:34:14 +0800

    Update database to fetch from api periodically

--- server/app.js
+++ server/app.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const cron = require('node-cron')
 const carParkRouter = require('./controllers/carPark')
+const timeRouter = require('./controllers/time')
 const databaseService = require('./services/database')
 const middleware = require('./utils/middleware')
 
 databaseService.connect()
 if (process.env.NODE_ENV !== 'test') {
   databaseService.update()
+  cron.schedule('*/1 * * * *', () => {
+    databaseService.update()
+  })
 }
 
 app.use(cors())
@@ -15,6 +20,7 @@ app.use(express.static('build'))
 app.use(express.json())
 
 app.use('/api/carPark', carParkRouter)
+app.use('/api/time', timeRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)

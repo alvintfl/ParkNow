058a453ee5bc111e58c484db0fbc11e6c5582658
alvintfl <alvintanfulong@gmail.com>
2023-08-14 23:41:39 +0800

    Change form to search while typing instead

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -3,18 +3,17 @@ import { useState } from 'react'
 const SearchBar = ({ getCarPark }) => {
   const [carParkCode, setCarParkCode] = useState('')
 
-  const search = async (e) => {
-    e.preventDefault()
-    getCarPark(carParkCode)
-    setCarParkCode('')
+  const search = async (location) => {
+    getCarPark(location)
+    setCarParkCode(location)
   }
 
   return (
     <div>
-      <form onSubmit={search}>
+      <form onSubmit={e => e.preventDefault()}>
         <input
           value={carParkCode}
-          onChange={({ target }) => setCarParkCode(target.value)}
+          onChange={({ target }) => search(target.value)}
           placeholder='Enter a location'
         />
       </form>

1cbe54e426760d437d82545eb64ce94468f37719
alvintfl <alvintanfulong@gmail.com>
2023-08-14 21:46:23 +0800

    Update GET endpoint to escape special characters

--- server/controllers/carPark.js
+++ server/controllers/carPark.js
@@ -8,8 +8,11 @@ carParkRouter.get('/', async (request, response) => {
 })
 
 carParkRouter.get('/:development', async (request, response) => {
-  const carPark = await CarPark.find({ development: new RegExp(request.params.development, 'i')}) 
+  const filteredDevelopment = escapeRegExp(request.params.development)
+  const carPark = await CarPark.find({ development: new RegExp(filteredDevelopment, 'i')}) 
   response.json(carPark)
 })
+
+const escapeRegExp = string => string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 
 module.exports = carParkRouter

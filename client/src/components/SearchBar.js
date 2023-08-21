18e465557173b49582851bd676897e0b0a70f965
alvintfl <alvintanfulong@gmail.com>
2023-08-21 22:14:12 +0800

    Change TextField to InputBase for more customizability

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,24 +1,41 @@
 import { useState } from 'react'
-import { TextField } from '@mui/material'
+import { Paper, InputBase } from '@mui/material'
 import SearchButton from './SearchButton'
 
 const SearchBar = ({ getCarPark }) => {
   const [carParkCode, setCarParkCode] = useState('')
 
   const search = async (e) => {
+    console.log(e)
     e.preventDefault()
     getCarPark(carParkCode)
   }
 
+  const style = {
+    p: '0.3rem 0.3rem 0.3rem 0.5rem',
+    margin: '0.25rem',
+    display: 'flex',
+    alignItems: 'center',
+    height: '2.5rem',
+    width: '13rem'
+  }
+
   return (
-    <form onSubmit={search}>
-      <TextField variant='outlined'
+    <Paper
+      component="form"
+      sx={style}
+      onSubmit={search}
+      elevation={0}
+      variant='outlined'
+    >
+      <InputBase
         placeholder='Enter a location'
         onChange={({ target }) => setCarParkCode(target.value)}
         size='small'
+        inputProps={{ style: { fontSize: 14 } }}
       />
       <SearchButton search={search} />
-    </form>
+    </Paper>
   )
 }
 
